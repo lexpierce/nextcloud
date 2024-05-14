e7fcf38cf0b038e97da7e44ef77cd11cd4f2a520
Alexander Sisco <alexander@render.com>
2024-05-14 12:45:04 -0700

    experiment user

--- nextcloud/trusted_domains.sh
+++ nextcloud/trusted_domains.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-chsh -s /usr/bin/bash www-data
-su -c "php occ config:system:set trusted_domains 1 --value=${RENDER_EXTERNAL_HOSTNAME}" www-data
-chsh -s /usr/bin/nologin www-data
+php occ config:system:set trusted_domains 1 --value=${RENDER_EXTERNAL_HOSTNAME}
+php occ config:system:get trusted_domains
 
-echo "Added $RENDER_EXTERNAL_HOSTNAME to trusted domains"
